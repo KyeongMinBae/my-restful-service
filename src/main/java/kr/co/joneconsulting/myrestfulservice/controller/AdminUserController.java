30760f20d04501960ae63f81cb5fe1a49ee2d166
KyeongMinBae <reaaaaaaal@gmail.com>
2026-08-25 06:39:39 +0900

    30. Level3 단계의 REST API 구현을 위한 HATEOAS 적용

--- src/main/java/kr/co/joneconsulting/myrestfulservice/controller/AdminUserController.java
+++ src/main/java/kr/co/joneconsulting/myrestfulservice/controller/AdminUserController.java
@@ -31,7 +31,10 @@ public class AdminUserController {
     }
 
     // --> /admin/v1/users/{id}
-    @GetMapping("v1/users/{id}")
+//    @GetMapping("v1/users/{id}")
+//    @GetMapping(value = "/users/{id}", params = "version=1")
+//    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=1")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv1+json")
     public AdminUser retrieveUser4Admin(@PathVariable int id) {
         User user = service.findOne(id);
 
@@ -52,7 +55,10 @@ public class AdminUserController {
     }
 
     // --> /admin/v2/users/{id}
-    @GetMapping("v2/users/{id}")
+//    @GetMapping("v2/users/{id}")
+//    @GetMapping(value = "/users/{id}", params = "version=2")
+//    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=2")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv2+json")
     public AdminUser retrieveUser4AdminV2(@PathVariable int id) {
         User user = service.findOne(id);
 
--- src/main/java/kr/co/joneconsulting/myrestfulservice/controller/UserController.java
+++ src/main/java/kr/co/joneconsulting/myrestfulservice/controller/UserController.java
@@ -1,17 +1,24 @@
 package kr.co.joneconsulting.myrestfulservice.controller;
 
+import jakarta.persistence.EntityManager;
 import jakarta.validation.Valid;
 import kr.co.joneconsulting.myrestfulservice.bean.User;
 import kr.co.joneconsulting.myrestfulservice.dao.UserDaoService;
 import kr.co.joneconsulting.myrestfulservice.exception.UserNotFoundException;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.swing.text.html.parser.Entity;
 import java.net.URI;
 import java.net.http.HttpResponse;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
@@ -29,14 +36,19 @@ public class UserController {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
 
         if (user == null) {
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
         }
 
-        return user;
+        EntityModel entityModel = EntityModel.of(user);
+
+        WebMvcLinkBuilder linTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(linTo.withRel("all-users")); // all-users -> http://localhost:8088/users
+
+        return entityModel;
     }
 
     @PostMapping("/users")
