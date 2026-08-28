6323976b803b2d95d8d3ed2cbee0721580d2548d
KyeongMinBae <reaaaaaaal@gmail.com>
2026-08-28 21:10:25 +0900

    42. Spring Data JPA를 이용한 초기 데이터 생성

--- src/main/java/kr/co/joneconsulting/myrestfulservice/config/SecurityConfig.java
+++ src/main/java/kr/co/joneconsulting/myrestfulservice/config/SecurityConfig.java
@@ -2,30 +2,38 @@ package kr.co.joneconsulting.myrestfulservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.util.AntPathMatcher;
 
-//@Configuration
+@Configuration
 public class SecurityConfig {
 
-//    @Bean
-//    UserDetailsService userDetailsService() {
-//        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-//
-//        UserDetails newUser = User.withUsername("user")
-//                .password(passwordEncoder().encode("passw0rd"))
-//                .authorities("read")
-//                .build();
-//
-//        userDetailsManager.createUser(newUser);
-//        return userDetailsManager;
-//    }
-//
-//    @Bean
-//    BCryptPasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+    @Bean
+    UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+
+        UserDetails newUser = User.withUsername("user")
+                .password(passwordEncoder().encode("passw0rd"))
+                .authorities("read")
+                .build();
+
+        userDetailsManager.createUser(newUser);
+        return userDetailsManager;
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/h2-console/**");
+    }
 }
