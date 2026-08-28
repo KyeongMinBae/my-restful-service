903ef53fb7a7097dede9e492789bcee6801716fb
KyeongMinBae <reaaaaaaal@gmail.com>
2026-08-28 21:35:43 +0900

    44. JPA를 이용한 개별 사용자 상세 조회 - HTTP Get method

--- src/main/java/kr/co/joneconsulting/myrestfulservice/controller/UserJPAController.java
+++ src/main/java/kr/co/joneconsulting/myrestfulservice/controller/UserJPAController.java
@@ -1,12 +1,21 @@
 package kr.co.joneconsulting.myrestfulservice.controller;
 
+import kr.co.joneconsulting.myrestfulservice.exception.UserNotFoundException;
 import kr.co.joneconsulting.myrestfulservice.repository.UserRepository;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/jpa")
@@ -22,5 +31,22 @@ public class UserJPAController {
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return userRepository.findAll();
+    }
+
+    // /jpa/users/{id}
+    @GetMapping("/users/{id}")
+    public ResponseEntity retrieveUsersById(@PathVariable int id) {
+        Optional<User> user = userRepository.findById(id);
+
+        if (!user.isPresent()) {
+            throw new UserNotFoundException("id-" + id);
+        }
+
+        EntityModel entityModel = EntityModel.of(user.get());
+        WebMvcLinkBuilder linTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+
+        entityModel.add(linTo.withRel("all-users")); // all-users -> http://localhost:8088/users
+
+        return ResponseEntity.ok(entityModel);
     }
 }
